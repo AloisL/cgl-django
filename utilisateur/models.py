49414ee90990362bd5d51b09661d76e4733773d6
CChaudeurdy <chaudeurdy.corentin@hotmail.fr>
2021-01-10 18:00:28 +0100

    new modules

---
+++ utilisateur/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class Utilisateur(AbstractUser):
+    karma = models.IntegerField(default=0);
+    is_validator = models.BooleanField(default=False)
+    description_perso = models.TextField(max_length=200);
+
+
+    def __str__(self):
+        return self.username
