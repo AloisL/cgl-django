67c540e2399f94f25acc9c0a716cd41fed064abe
sebasviot <sebastien.viot.49@gmail.com>
2021-01-16 22:07:28 +0100

    models + gitignore

--- mopga/modules/projet/models.py
+++ mopga/modules/projet/models.py
@@ -14,27 +14,30 @@ class Projects(models.Model):
     description = models.CharField(max_length=5000)
     copyright = models.CharField(max_length=200)
     score = models.IntegerField()
-    deadline = models.DateField(null=False, input_formats=['%d/%m/%Y'])
+    beginDate = models.DateField(input_formats=['%d/%m/%Y'])
+    deadline = models.DateField(input_formats=['%d/%m/%Y'])
     completed = models.BooleanField(default=False)
+    donationGoal = models.IntegerField()
 
 
 class Comments(models.Model):
-    idProject = models.ForeingKey(Projects)
+    idProject = models.ForeingKey(Projects, on_delete=models.CASCADE)
+    idUser = models.ForeingKey(Users, on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
     content = models.CharField(max_length=500)
 
 
 class LeadBy(models.Model):
-    idProject = models.ForeingKey(Projects)
-    idUser = models.ForeingKey(Users)
+    idProject = models.ForeingKey(Projects, on_delete=models.CASCADE)
+    idUser = models.ForeingKey(Users, on_delete=models.CASCADE)
 
 
 class EvaluateBy(models.Model):
-    idProject = models.ForeingKey(Projects)
-    idUser = models.ForeingKey(Users)
+    idProject = models.ForeingKey(Projects, on_delete=models.CASCADE)
+    idUser = models.ForeingKey(Users, on_delete=models.CASCADE)
 
 
 class FundedBy(models.Model):
-    idProject = models.ForeingKey(Projects)
-    idUser = models.ForeingKey(Projects)
+    idProject = models.ForeingKey(Projects, on_delete=models.CASCADE)
+    idUser = models.ForeingKey(Projects, on_delete=models.CASCADE)
 
