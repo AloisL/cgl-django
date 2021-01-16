8712989c57ef90fc0b133523d4fade38b28ccac4
sebasviot <sebastien.viot.49@gmail.com>
2021-01-16 16:04:14 +0100

    "home"

--- home/models.py
+++ home/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    identifiant = models.CharField(max_length=200, unique=True)
+    motdepasse = models.CharField(max_length=200)
+    karma = models.IntegerField(default=0)
