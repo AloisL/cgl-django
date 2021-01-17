855de4b725f68f4917ecdd570706b7e111b48fb1
sebasviot <sebastien.viot.49@gmail.com>
2021-01-17 13:05:26 +0100

    register : add description and role + gitignore

--- mopga/modules/user/forms.py
+++ mopga/modules/user/forms.py
@@ -6,12 +6,15 @@ from mopga.modules.user.models import User
 
 class RegisterForm(UserCreationForm):
     username = forms.CharField(max_length=100)
+    description = forms.CharField(max_length=200, widget=forms.Textarea(attrs={'rows': 2}),
+                                  help_text='Write here a description of your project (200 caracters max.)');
+    CHOICES = [('', 'Pick a rule...'), (1, 'Maker'), (2, 'Funder'), (3, 'Rater')]
+    role = forms.ChoiceField(widget=forms.Select, choices=CHOICES)
     email = forms.EmailField(max_length=254)
     password1 = forms.PasswordInput()
     password2 = forms.PasswordInput()
 
-    # TODO : Ajouter type utilisateur
-
     class Meta:
         model = User
-        fields = UserCreationForm.Meta.fields + ('email',)
+        fields = UserCreationForm.Meta.fields + ('email', 'role', 'description')
+
--- mopga/modules/user/models.py
+++ mopga/modules/user/models.py
@@ -4,8 +4,18 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     karma = models.IntegerField(default=0)
-    is_validator = models.BooleanField(default=False)
-    description_perso = models.TextField(max_length=200)
+    role = models.IntegerField()
+    description = models.TextField(max_length=200)
+
+    def is_maker(self):
+        return self.role == 1
+
+    def is_funder(self):
+        return self.role == 2 | self.role == 3
+
+    def is_rater(self):
+        return self.role == 3 | self.role == 2
 
     def __str__(self):
         return self.username
+
