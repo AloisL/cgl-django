f30071243907819a3e289553fb15d719932a34d7
Aloïs LARDEUX <alois.lardeux@gmail.com>
2021-01-17 15:20:57 +0100

    typo role

--- mopga/modules/user/forms.py
+++ mopga/modules/user/forms.py
@@ -8,7 +8,7 @@ class RegisterForm(UserCreationForm):
     username = forms.CharField(max_length=100)
     description = forms.CharField(max_length=200, widget=forms.Textarea(attrs={'rows': 2}),
                                   help_text='Write here a description of your project (200 caracters max.)');
-    CHOICES = [('', 'Pick a rule...'), (1, 'Maker'), (2, 'Funder'), (3, 'Rater')]
+    CHOICES = [('', 'Pick a role...'), (1, 'Maker'), (2, 'Funder'), (3, 'Rater')]
     role = forms.ChoiceField(widget=forms.Select, choices=CHOICES)
     email = forms.EmailField(max_length=254)
     password1 = forms.PasswordInput()
@@ -17,4 +17,3 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = UserCreationForm.Meta.fields + ('email', 'role', 'description')
-
