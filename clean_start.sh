52eaab0db3bc9b8d679d7cde2c9a1eb9c12de0fe
Aloïs LARDEUX <alois.lardeux@gmail.com>
2021-01-17 15:21:16 +0100

    add start script

---
+++ clean_start.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+HOME_DIR=$(pwd)
+
+echo "Removing sb.sqlite3..."
+rm db.sqlite3
+
+cd mopga/modules/
+for d in */ ; do
+    echo "Removing db init files in module $d"
+    rm $d/migrations/0* 2> /dev/null
+done
+
+cd $HOME_DIR
+
+echo "Starting db makemigrations..."
+python manage.py makemigrations
+
+echo "Starting db migrate..."
+python manage.py migrate
+
+echo "Starting server..."
+python manage.py runserver
