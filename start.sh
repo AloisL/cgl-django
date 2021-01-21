1bc9f105ae05ccd783c34c17b68fda44a2eeeb7c
AloisL <46025556+AloisL@users.noreply.github.com>
2021-01-21 21:12:01 +0100

    bonus

--- start.sh
+++ start.sh
@@ -3,3 +3,5 @@
 docker stop $(docker ps -q --filter ancestor=mopga)
 docker build . -t mopga --no-cache
 docker run -t -p 8000:8000 mopga:latest &>/dev/null &
+sleep 5
+xdg-open http://127.0.0.1:8000/
